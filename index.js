6251a2e1231cb58381024320bc45ab69f91bcdce
riships <avviare.rishi@gmail.com>
2023-11-14 13:51:07 +0530

    watch

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("flowerData.json");
+// const router = jsonServer.router("userData.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 const fs = require('fs');
